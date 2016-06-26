72faa07f63f30bc6cf080ca3fa8bf92875294782
satbekmyrza <satbekmyrza@gmail.com>
2016-06-26 01:29:38 -0400

    removed greek letter names variable

--- geopar/angle.py
+++ geopar/angle.py
@@ -18,15 +18,9 @@ NOTES:
 
 __author__ = 'satbek'
 
-# allows support for up to len(GREEK_LETTERS) variables
+# allows support for up to len(GREEK_LETTERS) variables (currently 15)
 GREEK_LETTERS = 'αβγδεηθλπρστμφω'
 VAR_SUPPORT = len('αβγδεηθλπρστμφω')
-
-# total of 24 letters
-GREEK_LETTER_NAMES = {'α': 'alpha', 'β': 'beta', 'γ': 'gamma', 'δ': 'delta', 'ε': 'epsilon', 'ζ': 'zeta', 'η': 'eta',
-                      'θ': 'theta', 'ι': 'iota', 'κ': 'kappa', 'λ': 'lambda', 'μ': 'mu', 'ν': 'nu', 'ξ': 'xi',
-                      'ο': 'omicron', 'π': 'pi', 'ρ': 'rho', 'σ': 'sigma', 'τ': 'tau', 'υ': 'upsilon', 'φ': 'phi',
-                      'χ': 'chi', 'ψ': 'psi', 'ω': 'omega'}
 
 
 class Angle:
