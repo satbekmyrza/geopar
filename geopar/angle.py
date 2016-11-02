9d0fa351cf5ee819fcd2741ca4c34e1ffbc7dc7f
satbekmyrza <satbekmyrza@gmail.com>
2016-11-02 15:44:04 -0400

    test. added new issue

--- geopar/angle.py
+++ geopar/angle.py
@@ -13,7 +13,7 @@ ISSUES:
  8. [SOLVED] Angle: __truediv__ is not implemented
  9. Unknown Angle object has a dimension. Not necessary.
 10. [SOLVED] __str__ does not process unknown angle
-11. 
+11.
 
 NOTES:
 """
