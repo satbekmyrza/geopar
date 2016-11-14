316afb01f858bc3d9fef234a311126f51e0c4748
satbekmyrza <satbekmyrza@gmail.com>
2016-11-14 17:55:07 -0500

    missing tests added, existing tests refactored

--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,7 +1,6 @@
 import unittest
 from geopar.triangle import Triangle
 from geopar.angle import Angle
-from fractions import Fraction
 
 __author__ = 'satbek'
 
@@ -66,15 +65,44 @@ class TestTriangle(unittest.TestCase):
         self.assertTrue(30 in self.triangle1.get_angles())
         self.assertTrue(Angle.from_str('30 40 110') in self.triangle2.get_angles())
 
-    def test_angle_points_by_point(self):
-        self.assertEqual(self.triangle0.get_angle_points_by_point(1), [3, 1, 2])
-        self.assertEqual(self.triangle0.get_angle_points_by_point(2), [1 ,2, 3])
-        self.assertEqual(self.triangle0.get_angle_points_by_point(3), [2, 3, 1])
+    def test_get_points(self):
+        self.assertEqual(self.triangle0.get_points(), [1, 2, 3])
+        self.assertTrue(77 in self.triangle1.get_points())
+        self.assertFalse(78 in self.triangle1.get_points())
 
     def test_angle_of_point(self):
         self.assertTrue(20 == self.triangle0.angle_of_point(1))
         self.assertTrue(30 == self.triangle1.angle_of_point(77))
         self.assertTrue(Angle([60,60,60]) == self.triangle2.angle_of_point(88))
+
+        with self.assertRaises(Exception):
+            self.triangle1.angle_of_point(4)
+
+    def test_index_of_point(self):
+        self.assertTrue(0 == self.triangle0.index_of_point(1))
+        self.assertTrue(1 == self.triangle0.index_of_point(2))
+        self.assertTrue(2 == self.triangle0.index_of_point(3))
+
+        with self.assertRaises(Exception):
+            self.triangle1.angle_of_point(4)
+
+    def test_point_following(self):
+        self.assertEqual(3, self.triangle0.point_following(2))
+        self.assertEqual(1, self.triangle0.point_following(3))
+
+        with self.assertRaises(Exception):
+            self.triangle1.angle_of_point(4)
+
+    def test_point_preceding(self):
+        self.assertEqual(1, self.triangle1.point_preceding(77))
+
+        with self.assertRaises(Exception):
+            self.triangle1.angle_of_point(4)
+
+    def test_count_known(self):
+        self.assertEqual(3, self.triangle0.count_known())
+        self.assertEqual(3, self.triangle1.count_known())
+        self.assertEqual(2, self.triangle2.count_known())
 
     def test_has_point(self):
         self.assertTrue(self.triangle0.has_point(1))
@@ -82,15 +110,42 @@ class TestTriangle(unittest.TestCase):
         self.assertFalse(self.triangle0.has_point(4))
         self.assertTrue(self.triangle1.has_point(77))
 
-    def test_preceding_and_next_point(self):
-        self.assertEqual(3, self.triangle0.point_following(2))
-        self.assertEqual(1, self.triangle0.point_following(3))
-        self.assertEqual(1, self.triangle1.point_preceding(77))
+    def test_has_all_points(self):
+        self.assertTrue(self.triangle0.has_all_points([1, 2, 3]))
+        self.assertTrue(self.triangle1.has_all_points([1, 2, 77]))
 
-    def test_find_third_angle(self):
-        # self.assertTrue(self.triangle2.complete_unknown_angle())
-        pass
+    def test_has_unknown(self):
+        self.assertFalse(self.triangle0.has_unknown())
+        self.assertTrue(self.triangle2.has_unknown())
+
+    def test_get_angle_points_by_point(self):
+        self.assertEqual(self.triangle0.get_angle_points_by_point(1), [3, 1, 2])
+        self.assertEqual(self.triangle0.get_angle_points_by_point(2), [1, 2, 3])
+        self.assertEqual(self.triangle0.get_angle_points_by_point(3), [2, 3, 1])
+
+    def test_set_angle_by_point(self):
+        triangle = Triangle([1, 2, 3], [20, 30, 130])
+        triangle.set_angle_by_point(1, 30)
+        triangle.set_angle_by_point(2, 20)
+        self.assertEqual(30, triangle.angle_of_point(1))
+        self.assertEqual(20, triangle.angle_of_point(2))
+        self.assertEqual(130, triangle.angle_of_point(3))
+
+        with self.assertRaises(Exception):
+            self.triangle1.angle_of_point(4)
+
+    def test_set_angle_by_index(self):
+        triangle = Triangle([1, 2, 3], [20, 30, 130])
+        triangle.set_angle_by_index(0, 30)
+        triangle.set_angle_by_index(1, 20)
+        self.assertEqual(30, triangle.angle_of_point(1))
+        self.assertEqual(20, triangle.angle_of_point(2))
+        self.assertEqual(130, triangle.angle_of_point(3))
+
+        with self.assertRaises(Exception):
+            self.triangle1.set_angle_by_index(3)
 
     def test_complete_unknown_angle(self):
-        self.triangle3.complete_unknown_angle()
-        print(self.triangle3)
+        triangle = Triangle([1, 77, 88], [Angle([30, 40, 110]), Angle.from_str('x'), Angle([60, 60, 60])])
+        triangle.complete_unknown_angle()
+        self.assertEqual(Angle([-90, -100, 10]), triangle.angle_of_point(77))
